697e86bb12ddd3af7b794086442639c0e83c3155
estevaovilela <estevao.avilela@gmail.com>
2020-05-10 22:49:13 -0300

    script to collect covid data from Alberta, 2nd version with regex

--- scripts/06_covid_19_alberta.R
+++ scripts/06_covid_19_alberta.R
@@ -6,26 +6,20 @@ library(tidyverse)
 
 # Functions ---------------------------------------------------------------
 
-clean_data <- function(data_url, index, column) {
+clean_data <- function(raw) {
   
-  data <- data_url[index]
+  data <- raw
   data <- gsub("\"", replacement = "", x = data)
   data <- gsub(" ", replacement = "", x = data)
   data <- gsub("\\[", replacement = "", x = data)
   data <- str_split(data, pattern = ",")
-  if(column != 7) {
-    data <- data[[1]][-6158]
-    data
-  } else {
-    unlist(data)
-  }
+  data
 }
 
 # Reading -----------------------------------------------------------------
 
-# the HTML is read in lines, and each line is a element
-# in a vector
-covid_alberta_html <- readLines("/home/estevao/Desktop/helloworld.html")
+# the HTML is read in lines
+covid_alberta_html <- readLines("./data/raw/covid_alberta_10_05_2020.html")
 
 # Where is the data -------------------------------------------------------
 current_day <- Sys.Date()
@@ -33,35 +27,55 @@ current_day <- Sys.Date()
 occurrences <- grep(pattern = "data-for=\"htmlwidget-32e17777be5dc623828e\"", 
                     x = covid_alberta_html)
 
-# with mannual inspection we know the correct line is 5389, or the last one
-index_html <- rev(occurrences)[1]
-
-grep(pattern = "data", covid_alberta_html[occurrences])
+# with mannual inspection we know the correct line is 2255
+raw <- str_extract(string = covid_alberta_html[occurrences], 
+                   # everything enclosed in two brackets
+                   # the data is in 'list-of-lists'
+                   # data: [[1,2,3], [male, female]]
+                   pattern = "(\\[{2}.*\\]{2})")
 
 # so, we'll start getting the data in vectors, then 
-# we'll create a data frame. The firts columns (the row id)
-# is in the next element of the vector: index_html + 1, and so on.
+# we'll create a data frame. We capture each variable 
+# one-by-one and then remove it from the raw data
+# we only capture the first bracket
+# "Instead you can make your dot-star non-greedy, 
+# which will make it match as few characters as possible:"
+# https://stackoverflow.com/questions/2503413/regular-expression-to-stop-at-first-match
 
 # id row
-id_vec <- clean_data(data_url = covid_alberta_html, index = index_html + 1, column = 1)
+id_vec <- str_extract(string = raw, pattern = "(\\[{1}.*?\\]{1})")
+id_vec <- clean_data(id_vec)
+raw_treated <- str_remove(string = raw, pattern = "(\\[{1}.*?\\]{1})")
 
 # date reported
-date_vec <- clean_data(data_url = covid_alberta_html, index = index_html + 2, column = 2)
+date_vec <- str_extract(string = raw_treated, pattern = "(\\[{1}.*?\\]{1})")
+date_vec <- clean_data(date_vec)
+raw_treated <- str_remove(string = raw_treated, pattern = "(\\[{1}.*?\\]{1})")
 
 # zone
-zone_vec <- clean_data(data_url = covid_alberta_html, index = index_html + 3, column = 3)
+zone_vec <- str_extract(string = raw_treated, pattern = "(\\[{1}.*?\\]{1})")
+zone_vec <- clean_data(zone_vec)
+raw_treated <- str_remove(string = raw_treated, pattern = "(\\[{1}.*?\\]{1})")
 
 # gender
-gender_vec <- clean_data(data_url = covid_alberta_html, index = index_html + 4, column = 4)
+gender_vec <- str_extract(string = raw_treated, pattern = "(\\[{1}.*?\\]{1})")
+gender_vec <- clean_data(gender_vec)
+raw_treated <- str_remove(string = raw_treated, pattern = "(\\[{1}.*?\\]{1})")
 
 # age group
-age_vec <- clean_data(data_url = covid_alberta_html, index = index_html + 5, column = 5)
+age_vec <- str_extract(string = raw_treated, pattern = "(\\[{1}.*?\\]{1})")
+age_vec <- clean_data(age_vec)
+raw_treated <- str_remove(string = raw_treated, pattern = "(\\[{1}.*?\\]{1})")
 
 # case status
-case_status_vec <- clean_data(data_url = covid_alberta_html, index = index_html + 6, column = 6)
+case_status_vec <- str_extract(string = raw_treated, pattern = "(\\[{1}.*?\\]{1})")
+case_status_vec <- clean_data(case_status_vec)
+raw_treated <- str_remove(string = raw_treated, pattern = "(\\[{1}.*?\\]{1})")
 
 # case type
-case_type_vec <- clean_data(data_url = covid_alberta_html, index = index_html + 7, column = 7)
+case_type_vec <- str_extract(string = raw_treated, pattern = "(\\[{1}.*?\\]{1})")
+case_type_vec <- clean_data(case_type_vec)
+raw_treated <- str_remove(string = raw_treated, pattern = "(\\[{1}.*?\\]{1})")
 
 df <- tibble(id = id_vec ,
              date_reported = date_vec,
@@ -78,7 +92,7 @@ df <- df %>%
 
 write.csv(df, file = paste0("./data/treated/cases_alberta_canada_", 
                                                paste(rev(str_split(current_day, pattern = "-")[[1]]), collapse = "_"), 
-                                               "_covid_project_.csv") , 
+                                               "_covid_project.csv") , 
           row.names = FALSE)
   
 
